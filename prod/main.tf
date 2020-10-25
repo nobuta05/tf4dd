e6013c1a6163fa3e19a3af0f077a3d7da1d7597d
Keishi Sando <nobuta05@gmail.com>
2020-10-25 16:58:02 +0900

    modified

--- prod/main.tf
+++ prod/main.tf
@@ -25,7 +25,7 @@ resource "datadog_monitor" "cpumonitor_prod" {
   name = "CPU Monitor for Production"
   type = "metric alert"
   message = "CPU usage alert"
-  query = "avg(last_1m):avg:system.cpu.system{*} by {host} > 70"
+  query = "avg(last_1m):avg:system.cpu.system{*} by {host} > 80"
 
   tags = ["env:prod"]
 }
