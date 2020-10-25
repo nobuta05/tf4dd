8d50120766ffd7ec202e4cd249029adf6db507d5
Keishi Sando <nobuta05@gmail.com>
2020-10-25 16:53:58 +0900

    updated

--- prod/main.tf
+++ prod/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_version = "0.13.5"
+
+  required_providers {
+    datadog = {
+      source = "datadog/datadog"
+    }
+  }
+
+  backend "remote" {
+    organization = "keishi-sandbox"
+
+    workspaces {
+      name = "Production_for_tf4dd"
+    }
+  }
+}
+
 provider "datadog" {
   api_key = var.dd_api_key
   app_key = var.dd_app_key
